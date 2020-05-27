82d6a86e61ce0f03d3483b364480679e40e8dfaa
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 11:57:41 +0300

    Use RequiredArgsConstructor

--- src/main/java/net/VytskaLT/NPCLibrary/skin/NPCTextures.java
+++ src/main/java/net/VytskaLT/NPCLibrary/skin/NPCTextures.java
@@ -2,16 +2,13 @@ package net.VytskaLT.NPCLibrary.skin;
 
 import com.comphenix.protocol.wrappers.WrappedSignedProperty;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class NPCTextures {
 
     @Getter
     private final String value, signature;
-
-    public NPCTextures(String value, String signature) {
-        this.value = value;
-        this.signature = signature;
-    }
 
     public WrappedSignedProperty toProperty() {
         return new WrappedSignedProperty("textures", value, signature);
