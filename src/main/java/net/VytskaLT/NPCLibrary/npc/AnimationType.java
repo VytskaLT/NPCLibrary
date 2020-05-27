1f9ed7accb074fab3ce0b96a5bb207827c3803a9
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 11:51:24 +0300

    Use RequiredArgsConstructor

--- src/main/java/net/VytskaLT/NPCLibrary/npc/AnimationType.java
+++ src/main/java/net/VytskaLT/NPCLibrary/npc/AnimationType.java
@@ -1,11 +1,10 @@
 package net.VytskaLT.NPCLibrary.npc;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum AnimationType {
     SWING(0), TAKE_DAMAGE(1), LEAVE_BED(2), EAT_FOOD(3), CRITICAL_EFFECT(4), MAGIC_CRITICAL_EFFECT(5);
 
     public final int id;
-
-    AnimationType(int id) {
-        this.id = id;
-    }
 }
